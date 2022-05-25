51a9ff0280663123c4c2f645ce41382094a6f2d5
Joey Brown <joseph.d.bronw94@gmail.com>
2022-05-25 09:31:25 -0400

    reworked routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import { Routes, Route } from "react-router-dom";
-import './App.css';
+import React from "react";
+import HomePage from "./Pages/homePage";
+import ResumePage from "./Pages/resumePage";
+import Header from "./Layout/Header";
+import NavBar from "./Layout/NavBar";
+import Footer from "./Layout/Footer";
 
-import HomePage from './Pages/homePage';
-import ResumePage from './Pages/resumePage';
-import Header from './Layout/Header';
-import NavBar from './Layout/NavBar';
-import Footer from './Layout/Footer';
+import "./App.css";
+
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
       <NavBar />
       <Header />
+      <HomePage />
+      <Footer />
+
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/resume" element={<ResumePage />} />
       </Routes>
-      <Routes>
-        <Route path='/resume' element={<ResumePage />} />
-      </Routes>
-      <Footer />
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 // Importing the Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 ReactDOM.render(
   <React.StrictMode>
